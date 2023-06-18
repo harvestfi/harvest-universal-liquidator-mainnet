ba59701054f8605dc3917396fda943cd43be4397
ryanycw <ryan@ryanycw.dev>
2023-06-19 00:31:38 +0900

    chore: format contracts

--- src/interfaces/curve/ICurveRegistryExchange.sol
+++ src/interfaces/curve/ICurveRegistryExchange.sol
@@ -26,9 +26,15 @@ interface ICurveRegistryExchange {
         view
         returns (address, uint256);
 
-    function get_exchange_amount(address _pool, address _from, address _to, uint256 _amount) external view returns (uint256);
+    function get_exchange_amount(address _pool, address _from, address _to, uint256 _amount)
+        external
+        view
+        returns (uint256);
 
-    function get_input_amount(address _pool, address _from, address _to, uint256 _amount) external view returns (uint256);
+    function get_input_amount(address _pool, address _from, address _to, uint256 _amount)
+        external
+        view
+        returns (uint256);
 
     function get_exchange_amounts(address _pool, address _from, address _to, uint256[100] memory _amounts)
         external
--- src/interfaces/uniswap/v2/IUniswapV2Router02.sol
+++ src/interfaces/uniswap/v2/IUniswapV2Router02.sol
@@ -96,13 +96,21 @@ interface IUniswapV2Router02 {
         payable
         returns (uint256[] memory amounts);
 
-    function swapTokensForExactETH(uint256 amountOut, uint256 amountInMax, address[] calldata path, address to, uint256 deadline)
-        external
-        returns (uint256[] memory amounts);
+    function swapTokensForExactETH(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
 
-    function swapExactTokensForETH(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline)
-        external
-        returns (uint256[] memory amounts);
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
 
     function swapETHForExactTokens(uint256 amountOut, address[] calldata path, address to, uint256 deadline)
         external
@@ -111,13 +119,25 @@ interface IUniswapV2Router02 {
 
     function quote(uint256 amountA, uint256 reserveA, uint256 reserveB) external pure returns (uint256 amountB);
 
-    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) external pure returns (uint256 amountOut);
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
+        external
+        pure
+        returns (uint256 amountOut);
 
-    function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut) external pure returns (uint256 amountIn);
+    function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut)
+        external
+        pure
+        returns (uint256 amountIn);
 
-    function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory amounts);
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
 
-    function getAmountsIn(uint256 amountOut, address[] calldata path) external view returns (uint256[] memory amounts);
+    function getAmountsIn(uint256 amountOut, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
 
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
