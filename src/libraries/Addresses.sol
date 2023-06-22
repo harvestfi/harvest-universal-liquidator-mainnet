e4dff18bb4e31f3efefb37b952ad9046b4a09221
ryanycw <ryan@ryanycw.dev>
2023-06-23 00:12:46 +0900

    chore: adjust name and add constants for bancor

--- src/libraries/Addresses.sol
+++ src/libraries/Addresses.sol
@@ -2,8 +2,13 @@
 pragma solidity 0.8.17;
 
 library Addresses {
-    address constant uniswapV3Router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    address constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
-    address constant sushiSwapRouter = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
-    address constant curveRouter = 0x99a58482BD75cbab83b27EC03CA68fF489b5788f;
+    // Routers or Registries
+    address constant _UNISWAPV3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address constant _BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    address constant _SUSHISWAP_ROUTER = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+    address constant _CURVE_ROUTER = 0x99a58482BD75cbab83b27EC03CA68fF489b5788f;
+    address constant _BANCOR_REGISTRY = 0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4;
+    // Tokens
+    address constant _WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant _BANCOR_ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 }
