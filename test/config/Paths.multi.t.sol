face2ba3d4f078a78ed17750fcb7a4df2286ca06
ryanycw <ryan@ryanycw.dev>
2023-06-23 19:23:17 +0900

    test: add testing paths in configs

--- test/config/Paths.multi.t.sol
+++ test/config/Paths.multi.t.sol
@@ -43,15 +43,15 @@ abstract contract MultiSwapPaths {
         newTokenPair.whale = 0x0990165a42B2c4fc00B71e5dbaA5Be6B3B11c953;
         newTokenPair.dexSetup.push(Types.DexSetting("SushiswapDex", _path));
 
-        // Pair3 - DAI -> USDT -> FRAX
-        _path[0] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        _path[1] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-        _path[2] = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
+        // Pair3 - FARM -> BNT -> WETH
+        _path = new address[](2);
+        _path[0] = 0xa0246c9032bC3A600820415aE600c6388619A14D;
+        _path[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
         newTokenPair = _multiTokenPairs[_multiTokenPairCount++];
-        newTokenPair.sellToken = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        newTokenPair.buyToken = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
-        newTokenPair.whale = 0x8Ce71EF87582B28De89D14970D00b2377F93F32b;
-        newTokenPair.dexSetup.push(Types.DexSetting("CurveDex", _path));
+        newTokenPair.sellToken = 0xa0246c9032bC3A600820415aE600c6388619A14D;
+        newTokenPair.buyToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        newTokenPair.whale = 0x49d71131396F23F0bCE31dE80526D7C025981c4d;
+        newTokenPair.dexSetup.push(Types.DexSetting("BancorV2Dex", _path));
     }
 }
