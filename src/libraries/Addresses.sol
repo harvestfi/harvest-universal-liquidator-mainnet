29503436a4d368bfc2578ef4990828ef58a231f1
ryanycw.eth <63644531+ryanycw@users.noreply.github.com>
2023-09-04 22:00:16 +0800

    build: core contracts (#2)
    * refactor: remove unused files
    
    * chore: init environment settings
    
    * build: create CI action
    
    * build: init contracts
    
    * chore: install packages
    
    * chore: adjust gitignore
    
    * docs: create pr template
    
    * chore: setup commitlint
    
    * fix: adjust codecov project name
    
    * chore: adjust router addresses
    
    * chore: format contracts

---
+++ src/libraries/Addresses.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+library Addresses {
+    address constant uniswapV3Router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    address constant sushiSwapRouter = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+    address constant curveRouter = 0x99a58482BD75cbab83b27EC03CA68fF489b5788f;
+}
