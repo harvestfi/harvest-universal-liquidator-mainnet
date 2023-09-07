7cdaaae91c538ef389230e121d98920c9361ad52
ryanycw <ryan@ryanycw.dev>
2023-09-07 22:53:06 +0800

    test: add afterswap test

--- test/dexes/MockDex.t.sol
+++ test/dexes/MockDex.t.sol
@@ -19,15 +19,14 @@ contract MockDexTest is AdvancedFixture {
         address testToken = 0xdAC17F958D2ee523a2206206994597C13D831ec7; //USDT
         address testWhale = 0x276cdBa3a39aBF9cEdBa0F1948312c0681E6D5Fd;
         changePrank(testWhale);
-        (bool success, bytes memory data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
-        assertTrue(success);
+        (, bytes memory data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
         uint256 balance = abi.decode(data, (uint256));
-        (success, data) = testToken.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance));
-        (success, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", address(mockDex)));
+        (, data) = testToken.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance));
+        (, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", address(mockDex)));
         assertEq(balance, abi.decode(data, (uint256)));
         changePrank(_governance);
         mockDex.tokenWithdraw(testToken, balance);
-        (success, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", _governance));
+        (, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", _governance));
         assertEq(balance, abi.decode(data, (uint256)));
     }
 
@@ -35,13 +34,44 @@ contract MockDexTest is AdvancedFixture {
         address testToken = 0xdAC17F958D2ee523a2206206994597C13D831ec7; //USDT
         address testWhale = 0x276cdBa3a39aBF9cEdBa0F1948312c0681E6D5Fd;
         changePrank(testWhale);
-        (bool success, bytes memory data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
-        assertTrue(success);
+        (, bytes memory data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
         uint256 balance = abi.decode(data, (uint256));
-        (success, data) = testToken.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance));
-        (success, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", address(mockDex)));
+        (, data) = testToken.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance));
+        (, data) = testToken.call(abi.encodeWithSignature("balanceOf(address)", address(mockDex)));
         assertEq(balance, abi.decode(data, (uint256)));
         vm.expectRevert("Ownable: caller is not the owner");
         mockDex.tokenWithdraw(testToken, balance);
+    }
+
+    function testAfterSwapBalance() public {
+        address testToken1 = 0xdAC17F958D2ee523a2206206994597C13D831ec7; //USDT
+        address testWhale = 0x276cdBa3a39aBF9cEdBa0F1948312c0681E6D5Fd;
+        changePrank(testWhale);
+        (, bytes memory data) = testToken1.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
+        uint256 balance1 = abi.decode(data, (uint256));
+        (, data) = testToken1.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance1));
+
+        address testToken2 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //USDC
+        testWhale = 0x5dBdeb65668075f42AA9eF8202Ee2B07A14EaF43;
+        changePrank(testWhale);
+        (, data) = testToken2.call(abi.encodeWithSignature("balanceOf(address)", testWhale));
+        uint256 balance2 = abi.decode(data, (uint256));
+        (, data) = testToken2.call(abi.encodeWithSignature("transfer(address,uint256)", address(mockDex), balance2));
+
+        address[] memory path = new address[](2);
+        path[0] = testToken1;
+        path[1] = testToken2;
+
+        changePrank(_governance);
+        // should fail if sell token balance > 0
+        vm.expectRevert();
+        mockDex.doSwap(0, 0, address(0), path);
+        mockDex.tokenWithdraw(testToken1, balance1);
+        // should fail if buy token balance > 0
+        vm.expectRevert();
+        mockDex.doSwap(0, 0, address(0), path);
+        mockDex.tokenWithdraw(testToken2, balance2);
+        // should success
+        mockDex.doSwap(0, 0, address(0), path);
     }
 }
