46c0279dbe95f055d44b0a8bd5cc9fd4179560d8
ryanycw <ryan@ryanycw.dev>
2023-09-07 19:43:44 +0800

    chore: add new fee settings for swap test

--- test/config/Fees.t.sol
+++ test/config/Fees.t.sol
@@ -15,7 +15,7 @@ abstract contract Fees {
         newFee.dexName = "UniV3Dex";
         newFee.fee = 500;
         // Pool1 - UNI -> USDC
-        _fees[_feePairsCount++];
+        newFee = _fees[_feePairsCount++];
         newFee.sellToken = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
         newFee.buyToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         newFee.dexName = "UniV3Dex";
@@ -26,5 +26,17 @@ abstract contract Fees {
         newFee.buyToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         newFee.dexName = "PancakeV3Dex";
         newFee.fee = 2500;
+        // Pool3 - AAVE -> WETH
+        newFee = _fees[_feePairsCount++];
+        newFee.sellToken = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+        newFee.buyToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        newFee.dexName = "UniV3Dex";
+        newFee.fee = 3000;
+        // Pool4 - WETH -> 1INCH
+        newFee = _fees[_feePairsCount++];
+        newFee.sellToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        newFee.buyToken = 0x111111111117dC0aa78b770fA6A738034120C302;
+        newFee.dexName = "UniV3Dex";
+        newFee.fee = 10000;
     }
 }
