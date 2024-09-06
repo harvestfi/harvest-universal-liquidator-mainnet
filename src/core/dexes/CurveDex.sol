0fb77013bbfc81337a34e115ec0bdbdc9369cec1
CryptJS13 <cryptjs13@protonmail.com>
2024-09-06 11:46:44 +0200

    feat: deploy curve dex

--- src/core/dexes/CurveDex.sol
+++ src/core/dexes/CurveDex.sol
@@ -9,7 +9,7 @@ import "./BasicDex.sol";
 
 // interfaces
 import "../../interfaces/ILiquidityDex.sol";
-import "../../interfaces/curve/ICurveRegistryExchange.sol";
+import "../../interfaces/curve/ICurveRouter.sol";
 
 // libraries
 import "../../libraries/Addresses.sol";
@@ -20,50 +20,65 @@ import {CurveDexStorage} from "../storage/CurveDex.sol";
 contract CurveDex is Ownable, BasicDex, ILiquidityDex, CurveDexStorage {
     using SafeERC20 for IERC20;
 
-    function doSwap(uint256 _sellAmount, uint256 _minBuyAmount, address _receiver, address[] calldata _path)
-        external
-        override
-        afterSwapCheck(_path[0], _path[_path.length - 1])
-        returns (uint256 receiveAmt)
-    {
-        uint256 sellAmount = _sellAmount;
-        uint256 minBuyAmount;
-        address receiver;
+    function doSwap(
+        uint256 _sellAmount,
+        uint256 _minBuyAmount,
+        address _receiver,
+        address[] memory _path
+    ) external override afterSwapCheck(_path[0], _path[_path.length - 1]) returns (uint256) {
+        IERC20(_path[0]).safeIncreaseAllowance(
+            Addresses._CURVE_ROUTER,
+            _sellAmount
+        );
 
-        for (uint256 idx; idx < _path.length - 1;) {
-            if (idx != _path.length - 2) {
-                minBuyAmount = 1;
-                receiver = address(this);
-            } else {
-                minBuyAmount = _minBuyAmount;
-                receiver = _receiver;
+        address[11] memory curvePath;
+        uint256[5][5] memory swapParams;
+        address[5] memory pools;
+        for (uint256 idx; idx < _path.length - 1; ) {
+            curvePath[idx*2] = _path[idx];
+            curvePath[idx*2+1] = pool(_path[idx], _path[idx+1]);
+            swapParams[idx] = params(_path[idx], _path[idx+1]);
+            pools[idx] = pool(_path[idx], _path[idx+1]);
+
+            if (idx == _path.length - 2) {
+                curvePath[idx*2+2] = _path[idx+1];
             }
 
-            address sellToken = _path[idx];
-            address buyToken = _path[idx + 1];
-            IERC20(sellToken).safeIncreaseAllowance(Addresses._CURVE_ROUTER, sellAmount);
-
-            receiveAmt = ICurveRegistryExchange(Addresses._CURVE_ROUTER).exchange(
-                _pool[sellToken][buyToken], sellToken, buyToken, sellAmount, minBuyAmount, receiver
-            );
-
-            sellAmount = IERC20(buyToken).balanceOf(address(this));
             unchecked {
                 ++idx;
             }
         }
+        ICurveRouter(Addresses._CURVE_ROUTER).exchange(
+            curvePath,
+            swapParams,
+            _sellAmount,
+            _minBuyAmount,
+            pools,
+            _receiver
+        );
     }
 
-    function setPool(address _token0, address _token1, address _poolAddr) external onlyOwner {
+    function pairSetup(
+        address _token0,
+        address _token1,
+        address _poolAddr,
+        uint256[5] calldata __params
+    ) external onlyOwner {
         _pool[_token0][_token1] = _poolAddr;
-        _pool[_token1][_token0] = _poolAddr;
+        _params[_token0][_token1] = __params;
     }
 
-    function pool(address _token0, address _token1) public view returns (address) {
+    function pool(
+        address _token0,
+        address _token1
+    ) public view returns (address) {
         return _pool[_token0][_token1];
     }
 
-    function tokenWithdraw(address _token, uint256 _amount) public override onlyOwner {
-        super.tokenWithdraw(_token, _amount);
+    function params(
+        address _token0,
+        address _token1
+    ) public view returns (uint256[5] memory) {
+        return _params[_token0][_token1];
     }
 }
